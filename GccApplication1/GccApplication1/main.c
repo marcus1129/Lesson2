80661f9b3ac1c19ba1d2feb48cc0000d6b34aaeb
marcus1129 <49095847+marcus1129@users.noreply.github.com>
2022-02-17 16:37:54 +0100

    Interrupt implemented

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -5,6 +5,7 @@
  * Author : osch
  */ 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "I2C.h"  //include library for i2c driver
@@ -16,6 +17,8 @@
 void init(){
 	DDRK = 0x00; //A8-15
 	DDRG = 0x00; //D4
+	DDRE = 0x00; //E4
+	PORTE = 0xFF;
 	PORTK = 0xFF;
 	PORTG = 0xFF;
 }
@@ -25,21 +28,28 @@ void storeValue(char* values, uint8_t buffer, int* index){
 	*index += 1;
 }
 
+volatile int gate = 0;
+
 int main(void)
 {  
-	init();
 	_i2c_address = 0X78;
-  
+	
+	init();
 	I2C_Init();
 	InitializeDisplay();
 	print_fonts();
-
-	char storedValues[3];
-    int index = 0;
 	clear_display(); 
 	
+	EICRB = 0b00000010;
+	EIMSK = 0b00010000;
+	sei();
+	
+	char storedValues[3];
+	int index = 0;
+	
 	while (1){
-		if((PING & 0b00100000) == 0b00000000){
+		/* ------ NO INTERRUPT SOLUTION ------ */
+		/*if((PING & 0b00100000) == 0b00000000){
 			while((PING & 0b00100000) == 0b00000000){}
 			storeValue(storedValues, PINK, &index);
 		}
@@ -48,7 +58,23 @@ int main(void)
 				sendCharXY(storedValues[i], 0, i);
 			}
 			index = 0;
+		}*/
+		
+		/* ------ INTERRUPT SOLUTION ------ */
+		if(gate == 1){
+			_delay_ms(100);
+			clear_display();
+			storeValue(storedValues, PINK, &index);
+			for(int i = 0; i < index; i++){
+				sendCharXY(storedValues[i], 0, i);
+			}
+			if(index == 3){
+				index = 0;
+			}
+			gate = 0;
 		}
 	}
 }
-
+ISR(INT4_vect){
+	gate = 1;
+}
